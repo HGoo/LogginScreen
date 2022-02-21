c56bdb32c5cdf8147ca8820771584f72d0196a82
Nikolay <halemhood@icloud.com>
2022-02-21 17:54:35 +0300

    Setup app

---
+++ Controller/ResultViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ResultViewController.swift
+//  LogginScreen
+//
+//  Created by Николай Петров on 21.02.2022.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        navigationItem.hidesBackButton = false
+    }
+}
---
+++ Model/ValidateData.swift
@@ -0,0 +1,8 @@
+//
+//  ValidateData.swift
+//  LogginScreen
+//
+//  Created by Николай Петров on 21.02.2022.
+//
+
+import Foundation
